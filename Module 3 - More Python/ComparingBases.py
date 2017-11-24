6b38f50ce89fe83ab0f478fb69afcda673ba8fee
Philip Andersson <33291160+Bralmar@users.noreply.github.com>
2017-11-24 09:13:31 +0100

    Add files via upload

--- Module 3 - More Python/ComparingBases.py
+++ Module 3 - More Python/ComparingBases.py
@@ -3,29 +3,31 @@
 import sys
 import math
 
+
 def extract_sequence(x):
 	'''Reads the fasta-format file and spearates the sequence from the name'''
 	sequence=""
 	name=""
-	for line in x:
+	for line in x:	#extracts sequence and name of organism
 		if line.startswith('>'):
 			fullname=line.replace('\n','')
 			
-			if len(fullname)>10:
+			if len(fullname)>10:	#restricts the name length to 10 characters
 				name += fullname[0:10]
 		else:
 			sequence+=line
-	#print(sequence)
-	#if 'c' or 't' or 'a' or 'g' in sequence:
-	#	sys.stderr.write('Error')
-	#	sys.exit()
+	for n in sequence:			#Error if sequence contains lower case letters.
+		if n=='c' or n=='t' or n=='a' or n=='g':
+			sys.stderr.write('Error. Sequence contains lower case letters.')
+			sys.stdout.write('\n')
+			sys.exit()
 	return sequence, name
 
 
 
 
 def compvec(sequence):
-	''' Counts the number of bases in each string'''
+	''' Counts the number of bases in each string and returns list of base contetnts of each sequence.'''
 	
 	gcount=sequence.count('G')
 	ccount=sequence.count('C')
@@ -47,54 +49,63 @@ def compvec(sequence):
 
 
 def distance(cocklist): 
+	'''Takes a list of base contents of each sequence. Compares the distance between each sequence. '''
 	diff = []
 	diffList = []
 	SqrtList = []
 	totSqrtList=[]
 	sqrt = []
-	totSqrtList2 = []
+	SqrtList2 = []
 	for i in cocklist:
+		
 		for n in cocklist:
+			
 			for m in range(0,4):
-				diff = math.pow((i[m]-n[m]),2)
+				diff = math.pow((i[m]-n[m]),2)	#Calculates differences of each base, and squares the result. 
 				diffList.append(diff)
 				diff = []
-			SqrtVal=(math.sqrt(sum(diffList)*0.25))
+			SqrtVal=(math.sqrt(sum(diffList)*0.25))	#Summs up each differences and divides it with 4.
 			SqrtVal=round(SqrtVal, 4)
 			SqrtList.append(str(SqrtVal))
+			
 			diffList = []
 			SqrtVal = []
-	return SqrtList
+		
+		SqrtList2.append(SqrtList)
+		SqrtList = []
+	
+	return SqrtList2
 
 
 
 
 def main():
+	'''Main function which reads the input files, and calls the other functions. It also prints the output.'''
 	cocklist = []
 	totname = []
 	for file in sys.argv[1:]:
 		x = open(file, 'r')
+		#Functions that do all calculations
 		sequence, name=extract_sequence(x)
-
 		totList = compvec(sequence)
 		cocklist.append(totList)
 		totname.append(name)
 	
 	totSqrtList = distance(cocklist)
 	
-	totSqrtList=' '.join(totSqrtList)
-	totname=' '.join(totname)
-	print(totSqrtList)
-	print(totname)
+	x = 0
 
-	#print(totSqrtList)
+	for line in totSqrtList:	#inserts name into the first position of each element in the list.
 		
-#	x=0
-#	print(len(sys.argv[1:]))
-#	for n in range(0, len(sys.argv[1:])):
-#		print(totname[n], totSqrtList[x:x+len(sys.argv[1:])])
-#		x+=len(sys.argv[1:])	
-	return cocklist 
+		line.insert(0, totname[x])
+		x += +1
+	
+	sys.stdout.write(str(len(sys.argv[1:])))
+	sys.stdout.write('\n')	
+	sys.stdout.write('\n'.join([' '.join(['{:7}'.format(item) for item in row])  for row in totSqrtList]))#Prints the list in the phylip format. 
+
+	sys.stdout.write('\n')
+	return 
 
 main()
 
