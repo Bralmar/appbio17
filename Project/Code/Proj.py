0add50dc771d9026c591433cf7e6aefca000c50a
Philip Andersson <33291160+Bralmar@users.noreply.github.com>
2017-12-15 12:00:03 +0100

    Add files via upload

--- Project/Code/Proj.py
+++ Project/Code/Proj.py
@@ -1,32 +1,55 @@
 #!/usr/bin/env python2
 
+#there are more than 50% indels,
+#at least 50% of amino acids are unique, OK
+#no amino acid appears more than twice.
+
 import sys
 from Bio import SeqIO
 from Bio.Seq import Seq
-from Bio import motifs
-import re 
+import pdb
 
 
 def ReadFasta():
 	LetterList = []
 	CompList = []
+	VarList=[]
+	Columns=[]
+
+	#Opens file and extracts each sequence as a list in a list
 	with open(sys.argv[1],'rU') as ih:
 		for record in list(SeqIO.parse(ih, "fasta")):
-			#sys.stdout.write('>'+ str(record.id) + '\n'
 			for i in range(0, len(record.seq)):
 				LetterList.append(record.seq[i])
 				
-					
-					
+			CompList.append("".join(LetterList))
+			LetterList = []	
 
-				CompList.append(LetterList)
+		#Extracts each column for each sequence and saves in a list (Columns)
+		for i in range(0, len(record.seq)):
+			for m in range(len(CompList)):
+				VarList.append(CompList[m][i])
+			Columns.append("".join(VarList))
+			VarList=[]
 				
-				#''.join(CompList)
-			LetterList = ""
+	return Columns
 
-	#print(LetterList)
-	#print(CompList)		
-	return
+Columns = ReadFasta()
 
+def CompareColumns(Columns):
+	RemList = []
+	ShortList = []
 
-ReadFasta()
+	for i in range(len(Columns)):
+		RemList.append(''.join(set(Columns[i])))
+
+	
+	for m in range(len(RemList)):
+		if float(len(RemList[m])) / len(Columns[m]) >= 0.5 or Columns[m].count('-')/float(len(Columns[m])) >= 0.5 :
+			ShortList.append(Columns[m].replace(Columns[m],len(Columns[m])*' '))
+		else:			
+			ShortList.append(Columns[m])
+	#print(len(ShortList))
+	print(ShortList)
+
+CompareColumns(Columns)
